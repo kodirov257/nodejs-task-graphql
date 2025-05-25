a8557b9cd1eec8f7c0517cd9d2c7bd642b8b4ce9
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2025-05-25 23:07:25 +0500

    fix: fix change post input

--- src/routes/graphql/dto/change-post-input.ts
+++ src/routes/graphql/dto/change-post-input.ts
@@ -1,9 +1,9 @@
-import { GraphQLInputObjectType, GraphQLNonNull, GraphQLString } from 'graphql';
+import { GraphQLInputObjectType, GraphQLString } from 'graphql';
 
 export const ChangePostInput = new GraphQLInputObjectType({
   name: 'ChangePostInput',
   fields: {
-    title: { type: new GraphQLNonNull(GraphQLString) },
-    content: { type: new GraphQLNonNull(GraphQLString) },
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
   },
 });
