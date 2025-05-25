3d5d94e0472670fb16ab44668440373561adf7aa
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2025-05-25 19:21:07 +0500

    fix: fix get member type response

--- src/routes/graphql/schemas/query.ts
+++ src/routes/graphql/schemas/query.ts
@@ -1,24 +1,13 @@
-import { GraphQLList, GraphQLNonNull, GraphQLObjectType } from 'graphql';
+import { GraphQLObjectType } from 'graphql';
 
-import { MemberType } from '../types/member-type.js';
-import { MemberTypeEnum } from '../enums/member-type-enum.js';
 import { getMemberType, getMemberTypes } from './queries/member-type.js';
 import { getUser, getUsers } from './queries/users.js';
 
 export const query = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    memberTypes: {
-      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(MemberType))),
-      resolve: getMemberTypes,
-    },
-    memberType: {
-      type: new GraphQLNonNull(MemberType),
-      args: {
-        id: { type: new GraphQLNonNull(MemberTypeEnum) }
-      },
-      resolve: getMemberType,
-    },
+    memberTypes: getMemberTypes,
+    memberType: getMemberType,
     users: getUsers,
     user: getUser,
   }
